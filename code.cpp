202acffc4ffb8b39a6fe7eb9f0e13e7ed116b6aa
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-07-30 16:02:53 +0530

    CSES weird algorithm

--- code.cpp
+++ code.cpp
@@ -2,52 +2,34 @@
 using namespace std;
 #define ll long long
 void solve();
+void rec(ll n)
+{
+	cout<<n<<" ";
+	if(n==1)
+		return;
+	else
+	{
+		if(n%2==0)
+			rec(n/2);
+		else
+			rec(3*n+1);
+	}
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	int t;
-	cin>>t;
-	while(t--)
-		solve();
+	// int t;
+	// cin>>t;
+	// while(t--)
+	// 	solve();
+	solve();
 	return 0;
 }
 void solve()
 {
 	
-	ll n,count=0;
+	ll n;
 	cin>>n;
-	ll a[n];
-	for (auto &x: a) {
-      cin >> x;
-    }	
-	ll min=20000000000,minp=20000000000;
-	for(int i=0;i<n;i++)
-	{
-		if(a[i]<min)
-		{
-			min=a[i];
-		}
-	}
-	for(int i=0;i<n;i++)
-	{
-		if(a[i]<minp&&a[i]!=min)
-		{
-			minp=a[i];
-		}
-	}
-	if(minp>2*min)
-	{
-		for(int i=0;i<n;i++)
-		{
-			if(a[i]>=minp)
-				count++;
-		}
-		if(count!=0)
-			cout<<count<<"\n";
-		else
-			cout<<n<<"\n";
-	}
-	else
-    	cout<<n<<"\n";
+	rec(n);
 }
