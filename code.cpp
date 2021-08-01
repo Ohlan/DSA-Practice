159722dea902003eb2983bd38345b4696a61c0ab
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-01 22:37:07 +0530

    736 division 2 three little pigs

--- code.cpp
+++ code.cpp
@@ -3,51 +3,63 @@ using namespace std;
 #define ll long long
 #define vi vector<int>
 void solve();
+int binomialCoeff(int n, int k)
+{
+    int res = 1;
+ 
+    // Since C(n, k) = C(n, n-k)
+    if (k > n - k)
+        k = n - k;
+ 
+    // Calculate value of
+    // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+    for (int i = 0; i < k; ++i) {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+ 
+    return res;
+}
+ 
 void print(vi &a)
 {
 	for(auto x:a)
 		cout<<x<<" ";
 	cout<<"\n";
 }
-int noofways(int n)
-{
-	if(n<0)
-	{
-		return 0;
-	}
-	if(n==0)
-		return 1;
-	return noofways(n-1)+noofways(n-2)+noofways(n-3);
 
-}
-// void subsets(vi &a,vi subset,int i)
-// {
-// 	if(i==a.size())
-// 	{
-// 		print(subset);
-// 		return;
-// 	}
-// 	vi s=subset;
-// 	s.push_back(a[i]);
-// 	subsets(a,s,i+1);
-// 	subsets(a,subset,i+1);
-// }
 int main()
 {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	int t;
-	cin>>t;
-	while(t--)
-		solve();
-	// solve();
+	// int t;
+	// cin>>t;
+	// while(t--)
+		// solve();
+	solve();
 	return 0;
 }
 void solve()
 {
-	int n;
-	cin>>n;
-	int ans=0;
-	ans=noofways(n);
-	cout<<ans<<"\n";	
+	ll n,q;
+	cin>>n>>q;
+	while(q--)
+	{
+		ll w,ans=0;
+		cin>>w;
+		ll k=1; 
+		if(w>3)
+		{
+			k=w/3;
+			if(w%3!=0)
+				k++;
+		}
+		k=3*k;
+		while(k<=n*3)
+		{
+			ans=((ans%1000000007)+(binomialCoeff(k,w))%1000000007)%1000000007;
+			k=k+3;
+		}
+		cout<<ans<<"\n";
+	}	
 }
