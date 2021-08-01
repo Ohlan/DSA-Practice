2ddb6b898c4773f15f44a4cd705df5adff31ad53
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-01 16:50:41 +0530

    no of ways to reach n step

--- code.cpp
+++ code.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
+#define ll long long
 #define vi vector<int>
 void solve();
 void print(vi &a)
@@ -9,19 +9,19 @@ void print(vi &a)
 		cout<<x<<" ";
 	cout<<"\n";
 }
-// void nowofways(int n, int step, int &ans)
-// {
-// 	if(step>=n)
-// 	{
-// 		if(step==n)
-// 			ans++;
-// 		return;
-// 	}
-// 	nowofways(n,step+1,ans);
-// 	nowofways(n,step+2,ans);
-// 	nowofways(n,step+3,ans);
+void nowofways(int n, int step, int &ans)
+{
+	if(step>=n)
+	{
+		if(step==n)
+			ans++;
+		return;
+	}
+	nowofways(n,step+1,ans);
+	nowofways(n,step+2,ans);
+	nowofways(n,step+3,ans);
 
-// }
+}
 // void subsets(vi &a,vi subset,int i)
 // {
 // 	if(i==a.size())
@@ -47,33 +47,9 @@ int main()
 }
 void solve()
 {
-	ll y,x;
-	cin>>y>>x;
-	ll ans=1;
-	if(x>=y)
-	{
-		if(x%2!=0)
-		{
-			ll temp=x;
-			x=y;
-			y=temp;
-			ans=y*y+1-x;
-		}
-		else
-			ans=x+(x-1)*(x-2)+(y-1);
-	}
-	else
-	{
-		if(y%2!=0)
-		{
-			ll temp=x;
-			x=y;
-			y=temp;
-			ans=x+(x-1)*(x-2)+(y-1);
-		}
-		else
-			ans=y*y+1-x;
-	}
-	cout<<ans<<"\n";
-	
+	int n;
+	cin>>n;
+	int ans=0;
+	nowofways(n,0,ans);
+	cout<<ans<<"\n";	
 }
