3fdcbb9544af301ed68569f968d529b2211e62e5
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-10 14:41:36 +0530

    binary search iterative and recursive + ternay search

--- code.cpp
+++ code.cpp
@@ -3,27 +3,52 @@ using namespace std;
 #define ll long long
 #define vi vector<int>
 void solve();
-int doUnion(vi a, int n, vi b, int m)  {
-	set<int> st;
-	for(auto x:a)
-		st.insert(x);
-
-	for(auto x:b)
-		st.insert(x);
-
-	return st.size();        
-}
-int doIntersection(vi a, int n, vi b, int m)
+int binarysearch(vi a,int l,int r,int e)
 {
-	set<int> st;map<int,int>mp;
-	for(auto x:a)
-		mp[x]=1;
-
-	for(auto x:b)
-		if(mp.count(x)!=0)
-			st.insert(x);
-
-	return st.size();
+	if(l<=r)
+	{
+		int mid=(l+r)/2;
+		if(a[mid]==e)
+			return mid;
+		else if(a[mid]>e)
+			return binarysearch(a,l,mid-1,e);
+		else
+			return binarysearch(a,mid+1,r,e);
+	}
+	return -1;
+}
+int ternarysearch(vi a,int l,int r,int e)
+{
+	if(l<=r)
+	{
+		int mid1=l+(r-l)/3;
+		int mid2=r-(r-l)/3;
+		if(e==a[mid1])
+			return mid1;
+		else if(e==a[mid2])
+			return mid2;
+		else if(e<a[mid1])
+			return ternarysearch(a,l,mid1-1,e);
+		else if(e>a[mid2])
+			return ternarysearch(a,mid2+1,r,e);
+		else
+			return ternarysearch(a,mid1+1,mid2-1,e);
+	}
+	return -1;
+}
+int iterative_bs(vi a,int l,int r,int e)
+{
+	while(l<=r)
+	{
+		int mid=(l+r)/2;
+		if(a[mid]==e)
+			return mid;
+		else if(a[mid]<e)
+			l=mid+1;
+		else
+			r=mid-1;
+	}
+	return -1;
 }
 int main()
 {
@@ -38,13 +63,13 @@ int main()
 }
 void solve()
 {
-	int n,m;
-	cin>>n>>m;
+	int n;
+	cin>>n;
 	vi a(n);
 	for(auto &x:a)
 		cin>>x;
-	vi b(m);
-	for(auto &x:b)
-		cin>>x;
-	cout<<doIntersection(a,n,b,m)<<"\n";
+	sort(a.begin(),a.end());
+	cout<<binarysearch(a,0,n-1,85)<<"\n";
+	cout<<ternarysearch(a,0,n-1,85)<<"\n";
+	cout<<iterative_bs(a,0,n-1,85)<<"\n";
 }
