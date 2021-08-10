40e4cb50e7fe4c94ea588c749b2522a57a99529c
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-10 17:29:22 +0530

    missing no and repeating no in n elements using negative values at indices

--- code.cpp
+++ code.cpp
@@ -3,53 +3,7 @@ using namespace std;
 #define ll long long
 #define vi vector<int>
 void solve();
-int binarysearch(vi a,int l,int r,int e)
-{
-	if(l<=r)
-	{
-		int mid=(l+r)/2;
-		if(a[mid]==e)
-			return mid;
-		else if(a[mid]>e)
-			return binarysearch(a,l,mid-1,e);
-		else
-			return binarysearch(a,mid+1,r,e);
-	}
-	return -1;
-}
-int ternarysearch(vi a,int l,int r,int e)
-{
-	if(l<=r)
-	{
-		int mid1=l+(r-l)/3;
-		int mid2=r-(r-l)/3;
-		if(e==a[mid1])
-			return mid1;
-		else if(e==a[mid2])
-			return mid2;
-		else if(e<a[mid1])
-			return ternarysearch(a,l,mid1-1,e);
-		else if(e>a[mid2])
-			return ternarysearch(a,mid2+1,r,e);
-		else
-			return ternarysearch(a,mid1+1,mid2-1,e);
-	}
-	return -1;
-}
-int iterative_bs(vi a,int l,int r,int e)
-{
-	while(l<=r)
-	{
-		int mid=(l+r)/2;
-		if(a[mid]==e)
-			return mid;
-		else if(a[mid]<e)
-			l=mid+1;
-		else
-			r=mid-1;
-	}
-	return -1;
-}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -68,8 +22,22 @@ void solve()
 	vi a(n);
 	for(auto &x:a)
 		cin>>x;
-	sort(a.begin(),a.end());
-	cout<<binarysearch(a,0,n-1,85)<<"\n";
-	cout<<ternarysearch(a,0,n-1,85)<<"\n";
-	cout<<iterative_bs(a,0,n-1,85)<<"\n";
+	int repeated,missing;
+	for(int i=0;i<n;i++)
+	{
+		int temp=a[abs(a[i])-1];
+		if(temp<0)
+		{
+			repeated=abs(a[i]);
+		}
+		a[abs(a[i])-1]=-a[abs(a[i])-1];
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]>0)
+		{
+			missing=i+1;
+		}
+	}
+	cout<<repeated<<" "<<missing<<"\n";
 }
