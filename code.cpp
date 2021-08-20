112967e558c94781a173a7f096e404837a700a16
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-20 16:57:43 +0530

    left rotate by m most efficient

--- code.cpp
+++ code.cpp
@@ -6,16 +6,37 @@ using namespace std;
 #define ull unsigned long long
 typedef pair<int,int> ii;
 void solve();
-int trailingZeroesFact(int n)
+int gcd(int a,int b)
 {
-	int i=1;int ans=0;
-	while(i*5<=n)
-	{
-		i=i*5;
-		ans=ans+n/i;
-	}
-	return ans;
+	if(a==0)
+		return b;
+	return gcd(b%a,a);
 }
+void leftRotate(vi &arr, int d, int n)
+{
+    /* To handle if d >= n */
+    d = d % n;
+    int g_c_d = gcd(d, n);
+    for (int i = 0; i < g_c_d; i++) {
+        /* move i-th values of blocks */
+        int temp = arr[i];
+        int j = i;
+ 
+        while (1) {
+            int k = j + d;
+            if (k >= n)
+                k = k - n;
+ 
+            if (k == i)
+                break;
+ 
+            arr[j] = arr[k];
+            j = k;
+        }
+        arr[j] = temp;
+    }
+}
+ 
 
 int main()
 {
@@ -31,8 +52,27 @@ int main()
 }
 void solve()
 {
-	int n;
-	cin>>n;
-	
-	cout<<trailingZeroesFact(n)<<"\n";
+	int n,m;
+	cin>>n>>m;
+	vi a(n);
+	for(auto &x:a)
+		cin>>x;
+
+	leftRotate(a,m,n);
+	// while(rep--)
+	// {
+	// 	for(int j=0;j<gcd;j++)
+	// 	{
+	// 		int temp=a[0];
+	// 		for(int i=j;i<n-gcd;i+=gcd)
+	// 		{
+	// 			a[icd]=a[i];
+	// 		}
+	// 		a[n-1]=temp;
+	// 	}
+	// }
+
+	for(auto x: a)
+		cout<<x<<" ";
+	cout<<"\n";
 }
