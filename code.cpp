9f53ce45ce65539ab6f70623df59d195804a7996
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-21 12:16:38 +0530

    max index difference for a[j]>=a[i] efficient

--- code.cpp
+++ code.cpp
@@ -1,22 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define mod 1000000007
+#define MIN -1000000
 #define ll long long
 #define vi vector<int>
 #define ull unsigned long long
 typedef pair<int,int> ii;
 void solve(); 
-void rearrange(int a[],int n)
+int maximumIndex(int a[],int n)
 {
-	int max=n;
-	for(int i=0;i<n;i++)
+	int s[n];
+	s[0]=a[0];
+	for(int i=1;i<n;i++)
 	{
-		a[i]+=(a[a[i]]%max)*max;
+		s[i]=min(a[i],s[i-1]);
 	}
-	for(int i=0;i<n;i++)
+	int l[n];
+	l[n-1]=a[n-1];
+	for(int i=n-2;i>=0;i--)
 	{
-		a[i]/=max;
+		l[i]=max(a[i],l[i+1]);
 	}
+	int i=0,j=0,ans=0;
+	while(i<n&&j<n)
+	{
+		if(s[i]<=l[j])
+		{
+			ans=max(ans,j-i);
+			j++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	
+	return ans;
 }
 
 int main()
@@ -42,11 +61,6 @@ void solve()
 		cin>>a[i];
 	}
 
-	rearrange(a,n);
-
-	for(int i=0;i<n;i++)
-	{
-		cout<<a[i]<<" ";
-	}
+	cout<<maximumIndex(a,n)<<"\n";
 
 }
