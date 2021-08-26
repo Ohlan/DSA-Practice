960b9abc8d69c6b1069975d9e0f3b3d7e5ac4594
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-26 07:55:44 +0530

    amazon Q2 nary tree fastest component

--- code.cpp
+++ code.cpp
@@ -8,95 +8,58 @@ typedef pair<int,int> ii;
 typedef struct node
 {
 	int data;
-	node* next;
-	node* rm;
-	node(int data=0)
+	vector<node> components;
+	node(){}
+	node(int data)
 	{
 		this->data=data;
-		next=NULL;
-		rm=NULL;
-	}
+	}	
 }node;
 void solve();
-void printrmlist(node* head)
+ii getMax(node mainc,node &max)
 {
-	while(head)
+	if(mainc.components.size()==0)
 	{
-		cout<<head->data<<" ";
-		if(head->rm)
-			cout<<head->rm->data;
-		cout<<"\n";
-		head=head->next;
+		if(mainc.data>max.data)
+			max=mainc;
+		return make_pair(mainc.data,1);
 	}
-	cout<<"\n";
-}
+	int sum=0,parts=0;double sp=0;
+	for(int i=0;i<mainc.components.size();i++)
+	{
+		ii p=getMax(mainc.components[i],max);
+		double speed=(p.first)/(p.second);
+		if(speed>sp)
+		{
+			max=mainc.components[i];
+			sp=speed;
+		}
+		sum+=p.first;
+		parts+=p.second;
+	}
+	sum+=(mainc.data);parts++;
+	double speedm=sum/parts;
+	if(speedm>sp)
+	{
+		max=mainc;
+		sp=speedm;
+	}
+	return make_pair(sum,parts);
 
-node* clonelist(node* head)
+}
+node* getFastestComponent(node* root)
 {
-	node* h=head;
-	while(h)
+	if(root)
 	{
-		node* temp=h->next;
-		node* nw=new node(h->data);
-		h->next=nw;
-		h->next->next=temp;
-		h=temp;
+		node mainc=(*root);
+		node max=node(0);
+		
+		ii p=getMax(mainc,max);
+		node* res=&max;
+		return res;
 	}
-	h=head;
-	while(h)
-	{
-		h->next->rm=h->rm?h->rm->next:NULL;
-		h=h->next->next;
-	}
-	h=head;
-	node* clone=h->next;
-	while(h->next->next)
-	{
-		node* temp=h->next;
-		h->next=h->next->next;
-		temp->next=temp->next->next;
-		h=h->next;
-	}
-	h->next=NULL;
-	return clone;
+	return NULL;
 }
-// node* mergesortedlist(node* h1,node* h2)
-// {
-// 	node* head;
-// 	if(!h1)
-// 		return h2;
-// 	if(!h2)
-// 		return h1;
-// 	if((h1->data)>(h2->data))
-// 	{
-// 		head=h2;
-// 		h2=h2->next;
-// 	}
-// 	else
-// 	{
-// 		head=h1;
-// 		h1=h1->next;
-// 	}
-// 	node* temp=head;
-// 	while(h1!=NULL&&h2!=NULL)
-// 	{
-// 		if((h1->data)>(h2->data))
-// 		{
-// 			temp->next=h2;
-// 			temp=h2;
-// 			h2=h2->next;
-// 		}
-// 		else
-// 		{
-// 			temp->next=h1;
-// 			temp=h1;
-// 			h1->next;			
-// 		}
-// 	}
-
-// 	return head;
-// }
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -109,24 +72,25 @@ int main()
 	
 	return 0;
 }
+
 void solve()
 {
-	node* h1=new node(1);
-	node* h2=new node(2);
-	node* h3=new node(3);
-	node* h4=new node(4);
-	node* h5=new node(5);
-	h1->next=h2;
-	h2->next=h3;
-	h3->next=h4;
-	h4->next=h5;
-	h1->rm=h3;
-	h2->rm=h1;
-	h3->rm=h5;
-	h4->rm=h4;
-	h5->rm=h1;
-	printrmlist(h1);
-	node* head=clonelist(h1);
-	printrmlist(head);
-	printrmlist(h1);
+	node* root=new node(200);
+	node rl(120);
+	node rr(180);
+	node a,b,c,d,e,f;
+	a=node(110);
+	b=node(20);
+	c=node(30);
+	d=node(180);
+	e=node(150);
+	f=node(80);
+	rl.components={a,b,c};
+	rr.components={d,e,f};
+	root->components={rl,rr};
+
+	node* ans=getFastestComponent(root);
+	cout<<ans->data<<" "<<ans->components.size();
+
+
 }
