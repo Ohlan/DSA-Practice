4c1f66452f48d8ceba9fbaf87e2da2d5abe2a82f
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-08-27 10:02:33 +0530

    spiral level order traversal line by line BT

--- code.cpp
+++ code.cpp
@@ -1,131 +1,89 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define mod 1000000007
 #define ll long long
 #define vi vector<int>
+#define ull unsigned long long
+typedef pair<int,int> ii;
 typedef struct node
 {
 	int data;
-	int hd;
-	struct node* left;
-	struct node* right;
+	node* left;
+	node* right;
 	node(int data)
-    {
-        this->data = data;
-        left = right = NULL;
-    }
+	{
+		this->data=data;
+		left=right=NULL;
+	}
 }node;
-typedef struct dllnode
+void solve();
+void spiral(node* root)
 {
-	int data;
-	struct dllnode* prev;
-	struct dllnode* next;
-	dllnode(int data)
-    {
-        this->data = data;
-        prev = next = NULL;
-    }
-}dllnode;
-void solveBT();
-void levelorder(node* root)
-{
-	if(root==NULL)
+	if(!root)
 		return;
+
 	queue<node*> q;
 	q.push(root);
 	q.push(NULL);
-	while(q.empty()==false)
+	int flag=0;
+	while(!q.empty())
 	{
 		node* temp=q.front();
 		if(temp)
 		{
-			while(q.front()!=NULL)	
+			while(q.front()!=NULL)
 			{
 				cout<<temp->data<<" ";
-				if (temp->left != NULL) 
-		            q.push(temp->left); 
-
-		        if (temp->right != NULL) 
-		            q.push(temp->right); 
+				if(flag==0)
+				{
+					if(temp->right)
+					{
+						q.push(temp->right);
+					}
+					if(temp->left)
+					{
+						q.push(temp->left);
+					}
+				}
+				else
+				{
+					if(temp->left)
+					{
+						q.push(temp->left);
+					}
+					if(temp->right)
+					{
+						q.push(temp->right);
+					}
+				}
+				
 				q.pop();
 				temp=q.front();
 			}
 			cout<<"\n";
 			q.push(NULL);
 		}
+		if(flag==0)
+			flag=1;
+		else
+			flag=0;
 		q.pop();
 	}
-}
-node* LCA(node* root,int k1,int k2)
-{
-	if(!root)
-		return NULL;
-	if(root->data==k1||root->data==k2)
-		return root;
-
-	node* leftlca=LCA(root->left,k1,k2);
-	node* rightlca=LCA(root->right,k1,k2);
-	if(leftlca!=NULL&&rightlca!=NULL)
-		return root;
-	return (leftlca!=NULL)?leftlca:rightlca;
-}
-void BT2Dll(node* root,dllnode** head)
-{
-	if(root==NULL)
-	{
-		return;
-	}
-	static dllnode* prev=NULL;
-	BT2Dll(root->left,head);
-	dllnode* temp=new dllnode(root->data);
-
-	if(!prev)
-	{
-		temp->prev=NULL;
-		prev=temp;
-		(*head)=temp;
-	}
-	else
-	{
-		temp->prev=prev;
-		prev->next=temp;
-		prev=temp;
-	}
-
-	BT2Dll(root->right,head);
-}
-int height(node* root)
-{
-	if(root==NULL)
-		return 0;
-	return max(height(root->left),height(root->right))+1;
-
-}
-void printlist(dllnode* head)
-{
-	while(head)
-	{
-		cout<<head->data<<" ";
-		head=head->next;
-	}
-	cout<<"\n";
-}
-void inorder(node* root)
-{
-	if(root==NULL)
-		return;
-	inorder(root->left);
-	cout<<root->data<<" ";
-	inorder(root->right);
 }
 int main()
 {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-	solveBT();
+    
+	// int t;
+	// cin>>t;
+	// while(t--)
+		solve();
+	
 	return 0;
 }
-void solveBT()
+
+void solve()
 {
 	node *root = new node(6);
     root->left = new node(3);
@@ -134,12 +92,5 @@ void solveBT()
     root->left->left->right = new node(1);
     root->left->right = new node(5);
     root->left->right->left = new node(4); 
-	node* lca=LCA(root,1,7);
-	cout<<lca->data<<"\n";
-	lca=LCA(root,1,3);
-	cout<<lca->data<<"\n";
-	dllnode* head;
-	BT2Dll(root,&head);
-	printlist(head);
-	
+    spiral(root);
 }
