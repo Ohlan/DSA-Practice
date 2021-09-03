28ce2b38bf0dec8a3ebc648b4e4bff6c3aaf3e14
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-09-03 21:45:50 +0530

    celebrity problem in O(n) time n constant space without stack

--- code.cpp
+++ code.cpp
@@ -9,39 +9,31 @@ typedef pair<int,int> ii;
 void solve();
 
 
-	bool knows(vector<vector<int> >& M,int a,int b)
-    {
-        return M[a][b];
-    }
-    int findCelebrity(vector<vector<int> >& M, int n) 
-    {
-        stack<int> s;
-        for(int i=0;i<n;i++)
-            s.push(i);
-        
-        while(s.size()>1)
-        {
-            int a=s.top();
-            s.pop();
-            int b=s.top();
-            s.pop();
-            if(knows(M,a,b))
-                s.push(b);
-            else
-                s.push(a);
-            
-        }
-        int c=s.top();
-        
-        for(int i=0;i<n;i++)
-        {
-            if((i!=c)&&(knows(M,i,c)==false||knows(M,c,i)))
-                return -1;
-        }
-        
-        return c;
-        
-    }
+bool knows(vector<vector<int> >& M,int a,int b)
+{
+    return M[a][b];
+}
+int findCelebrity(vector<vector<int> >& M, int n)
+{
+	int i=0,j=n-1;
+	while(i<j)
+	{
+		if(knows(M,i,j))
+			i++;
+		else
+			j--;
+	}
+
+	int c=i;
+	for(int i=0;i<n;i++)
+	{
+		if(i!=c)
+			if(knows(M,c,i)||!knows(M,i,c))
+				return -1;
+	}
+	return c;
+}
+    
 
 
 int main()
