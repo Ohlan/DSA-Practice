923b7a361afaa3a943caf2df33342745d7b8b5da
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-09-06 19:41:45 +0530

    count n say digits

--- code.cpp
+++ code.cpp
@@ -6,50 +6,33 @@ using namespace std;
 #define ull unsigned long long
 typedef pair<int,int> ii;
 void solve();
-int maxPalindromeSubstr(string s) {
-    int n=s.length();
-    bool palin[n][n];
-    for(int i=0;i<n;i++)
+string countAndSay(int n) {
+    if(n==1)
+        return "1";
+    
+    string s=countAndSay(n-1);
+    int count=1,i=1;
+    int l=s.length();
+    string res="";
+    
+    while(i<l)
     {
-    	palin[i][i]=1;
+        if(s[i]==s[i-1])
+            count++;
+        else
+        {
+            string c=to_string(count);
+            res+=c;
+            res+=s[i-1];
+            count=1;
+        }
+        i++;
     }
-    int max=1,ind=0;
-    for(int i=0;i<n-1;i++)
-    {
-    	if(s[i]==s[i+1])
-    	{
-    		palin[i][i+1]=1;
-    		if(max==1)
-    		{
-    			max=2;
-    			ind=i;
-    		}
-    	}
-    	else
-    		palin[i][i+1]=0;
-
-    }
-    for(int k=3;k<=n;k++)
-    {
-    	for(int i=0;i<n-k+1;i++)
-    	{
-    		int j=i+k-1;
-    		if(palin[i+1][j-1]==1&&s[i]==s[j])
-    		{
-    			palin[i][j]=1;
-    			if(k>max)
-	    		{
-	    			max=k;
-	    			ind=i;
-	    		}
-    		}
-    		else
-    			palin[i][j]=0;
-    	}
-    }
-    string res=s.substr(ind,max);
-    cout<<res<<"\n";
-    return max;
+    string c=to_string(count);
+    res+=c;
+    res+=s[i-1];
+    
+    return res;
 }
 int main()
 {
@@ -65,7 +48,7 @@ int main()
 }
 void solve()
 {
-	string s;
-	cin>>s;
-	cout<<"max length of palindrome substring:"<<maxPalindromeSubstr(s)<<"\n";
+	int n;
+	cin>>n;
+	cout<<countAndSay(n)<<"\n";
 }
