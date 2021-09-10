a3e969ce4db949eb6d58cf7f5e6b82656ad9890e
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-09-10 11:44:42 +0530

    floor of sqrt in logn

--- code.cpp
+++ code.cpp
@@ -7,54 +7,27 @@ using namespace std;
 typedef pair<int,int> ii;
 void solve();
 
-int findLast(vector<int>&a,int start,int end,int e)
+int floorsqrt(int n)
 {
-	int n=end-start;
+	if(n==1||n==0)
+		return n;
+	int start=0,end=n,ans;
 	while(start<=end)
 	{
 		int mid=(start+end)/2;
-		if(a[mid]>e&&(a[mid]>a[mid+1]||mid==n))
+		if(mid*mid==n)
 			return mid;
-		else if(a[mid]<e)
-			end=mid-1;
-		else
+		else if(mid*mid<n)
+		{
+			ans=mid;
 			start=mid+1;
-	}
-	return -1;
-}
-int bsearch(vector<int>&a,int start,int end,int e)
-{
-	while(start<=end)
-	{
-		int mid=(start+end)/2;
-		if(a[mid]==e)
-			return mid;
-		else if(a[mid]>e)
+		}
+		else
 			end=mid-1;
-		else
-			start=mid+1;
 	}
-	return -1;
+	return ans;
 }
-int findElement(vector<int>&a,int e)
-{
-	int n=a.size();
-	int actualLast=findLast(a,0,n-1,a[n-1]);
-	int start=0,end=n-1,res=-1;
-	if(actualLast!=-1)
-	{
-		if(e>=a[0])
-			end=actualLast;
-		else
-			start=actualLast+1;
-		res=bsearch(a,start,end,e);
-	}
-	else
-	{
-		res=bsearch(a,start,end,e);
-	}
-	return res;
-}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -69,11 +42,7 @@ int main()
 }
 void solve()
 {
-	int n,e;	
-	cin>>n>>e;
-	vi a(n);
-	for(auto &x:a)
-		cin>>x;
-	int ind=findElement(a,e);
-	cout<<ind<<"\n";
+	int n;	
+	cin>>n;
+	cout<<floorsqrt(n)<<"\n";
 }
