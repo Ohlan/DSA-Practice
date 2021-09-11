450fd7a785e7eba43c0208733ef3cf9c1b99d450
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-09-11 10:13:54 +0530

    maximum sum such that no two elements are adjacent

--- code.cpp
+++ code.cpp
@@ -6,67 +6,19 @@ using namespace std;
 #define ull unsigned long long
 typedef pair<int,int> ii;
 void solve();
-typedef struct half
-{
-    int val;
-    int first;
-    int second;
-}half;
-bool compare(half &p1,half &p2)
-{
-    return (p1.val<=p2.val);
-}
-bool noCommon(half a, half b)
-{
-    if (a.first == b.first || a.first == b.second
-        || a.second == b.first || a.second == b.second)
-        return false;
-    return true;
-}
-vector<vector<int> > fourSum(vector<int> &a, int sum)
-{
-    int n=a.size();
-    int l=(n*(n-1))/2,k=0;
-    half pairs[l];
-    for(int i=0;i<n-1;i++)
+int FindMaxSum(vi &a, int n)
     {
-        for(int j=i+1;j<n;j++)
+        int exc=0,inc=a[0];
+        int excn;
+        for(int i=1;i<n;i++)
         {
-            pairs[k].val=a[i]+a[j];
-            pairs[k].first=i;
-            pairs[k].second=j;
-            k++;
+            excn=max(inc,exc);
+            inc=exc+a[i];
+            exc=excn;
         }
+       return max(inc,exc);
     }
 
-    sort(pairs,pairs+l,compare);
-    vector<vector<int> > res;
-    int i=0,j=l-1;
-    while(i<j)
-    {
-        if(pairs[i].val+pairs[j].val==sum)
-        {
-            
-            int x=pairs[i].first;
-            int y=pairs[i].second;
-            int c=pairs[j].first;
-            int d=pairs[j].second;
-            if(noCommon(pairs[i], pairs[j]))
-                res.push_back({a[x],a[y],a[c],a[d]});
-            // i++;
-            j--;
-        }
-        else if(pairs[i].val+pairs[j].val<sum)
-        {
-            i++;
-        }
-        else
-            j--;
-
-    }
-    return res; 
-
-}
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -86,11 +38,6 @@ void solve()
 	vi a(n);
 	for(auto &x:a)
 		cin>>x;
-	vector<vector<int> > res=fourSum(a,23);
-    for(int i=0;i<res.size();i++)
-    {
-        for(int j=0;j<res[i].size();j++)
-            cout<<res[i][j]<<" ";
-        cout<<"\n";
-    }
+    cout<<FindMaxSum(a,n)<<"\n";
+	
 }
