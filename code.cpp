2682d6d189eec97c0481940995c9d0f10c742c1e
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-09-11 13:05:47 +0530

    count of subarray with sum equal to 0

--- code.cpp
+++ code.cpp
@@ -6,76 +6,20 @@ using namespace std;
 #define ull unsigned long long
 typedef pair<int,int> ii;
 void solve();
-void merge(int a1[], int a2[], int n, int m)
+ll findSubarray(ll a[], int n ) {
+        map<ll,ll> mp;
+    ll sum=0,c=0;
+    for(int i=0;i<n;i++)
     {
-        int i=0,j=0,k=0;
-        int ma=max(a1[n-1],a2[m-1])+1;
-        while(i<n&&j<m)
-        {
-            if((a1[i]%ma)>(a2[j]%ma))
-            {
-                if(k<n)
-                {
-                    a1[k]=(a2[j]%ma)*ma+a1[k];
-                    j++;k++;
-                }
-                else
-                {
-                    a2[k-n]=(a2[j]%ma)*ma+a2[k-n];
-                    j++;k++;
-                }
-            }
-            else
-            {
-                if(k<n)
-                {
-                    a1[k]=(a1[i]%ma)*ma+a1[k];
-                    i++;k++;
-                }
-                else
-                {
-                    a2[k-n]=(a1[i]%ma)*ma+a2[k-n];
-                    i++;k++;
-                }
-                
-            }
-        }
-        while(i<n)
-        {
-            if(k<n)
-                {
-                    a1[k]=(a1[i]%ma)*ma+a1[k];
-                    i++;k++;
-                }
-                else
-                {
-                    a2[k-n]=(a1[i]%ma)*ma+a2[k-n];
-                    i++;k++;
-                }
-        }
-        while(j<m)
-        {
-            if(k<n)
-                {
-                    a1[k]=(a2[j]%ma)*ma+a1[k];
-                    j++;k++;
-                }
-                else
-                {
-                    a2[k-n]=(a2[j]%ma)*ma+a2[k-n];
-                    j++;k++;
-                }
-        }
-        i=0,j=0;
-        while(i<n)
-        {    
-            a1[i]=a1[i]/ma;i++;
-        }
-        while(j<m)
-        {   
-            a2[j]=a2[j]/ma;j++;
-        }
-
+        sum+=a[i];
+        if(sum==0)
+            c++;
+        if(mp.find(sum)!=mp.end())
+            c+=mp[sum];
+        
+        mp[sum]++;
+    }
+    return c;
     }
 
 int main()
@@ -92,23 +36,12 @@ int main()
 }
 void solve()
 {
-	int n;	
+	ll n;	
 	cin>>n;
-	int a[n],b[n];
+	ll a[n];
 	for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    
-    for(int i=0;i<n;i++)
-    {
-        cin>>b[i];
-    }
-    sort(a,a+n);
-    sort(b,b+n);
-    merge(a,b,n,n);
-	for(int i=0;i<n;i++)
-    {
-        cout<<a[i]<<" ";
-    }
+    cout<<findSubarray(a,n)<<"\n";
 }
