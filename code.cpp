e60a7986ed367f9a676e39bc951c02177390b3e7
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-09-12 13:55:05 +0530

    inversion count

--- code.cpp
+++ code.cpp
@@ -6,15 +6,56 @@ using namespace std;
 #define ull unsigned long long
 typedef pair<int,int> ii;
 void solve();
-long long findMinDiff(vector<long long> a, long long n, long long m){
-    sort(a.begin(),a.end());
-    long long mi=a[n-1];
-    for(int i=0;i<=n-m;i++)
-    {
-        mi=min(mi,a[i+m-1]-a[i]);
-    }
-    return mi;
-    }
+int merge(int arr[], int start,int mid,int end)
+{
+	int l=end-start+1;
+	int a[l];
+	int i=start,j=mid+1,k=0;
+	int c=0;
+	while(i<=mid&&j<=end)
+	{
+		if(arr[i]<=arr[j])
+		{
+			a[k]=arr[i];
+			i++;k++;
+
+		}
+		else
+		{
+			a[k]=arr[j];
+			j++,k++;
+			c+=mid-i+1;
+		}
+	}
+	while(i<=mid)
+	{
+		a[k]=arr[i];
+		i++;k++;
+	}
+	while(j<=end)
+	{
+		a[k]=arr[j];
+		j++,k++;
+	}
+	k=0;
+	for(int p=start;p<=end;p++,k++)
+		arr[p]=a[k];
+	return c;
+}
+
+
+int inversionCount(int arr[], int start,int end)
+{
+	if(start<end)
+	{
+		int mid=(start+end)/2;
+		int c1=inversionCount(arr,start,mid);
+		int c2=inversionCount(arr,mid+1,end);
+		int c3=merge(arr,start,mid,end);
+		return c1+c2+c3;
+	}
+	return 0;		
+}
 
 int main()
 {
@@ -33,11 +74,15 @@ void solve()
 	int n;	
 	cin>>n;
 
-	vector<ll> a(n);
+	int a[n];
 	for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    cout<<findMinDiff(a,n,5);
+    cout<<inversionCount(a,0,n-1)<<"\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
     cout<<"\n";
 }
