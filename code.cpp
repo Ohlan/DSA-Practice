07039b2f86b608b541b557b77e9a0dde221f935b
Pushpendra Ohlan <ohlanpushpender@gmail.com>
2021-10-28 20:56:58 +0530

    edit distance with dp

--- code.cpp
+++ code.cpp
@@ -12,16 +12,20 @@ typedef pair<int,int> ii;
 void solve();
 int editDistance(string s1,string s2,int m,int n)
 {
-	if(m==0)
-		return n;
-	if(n==0)
-		return m;
-	if(s1[m-1]==s2[n-1])
-		return editDistance(s1,s2,m-1,n-1);
-	else
-		return 1+min(editDistance(s1,s2,m-1,n-1),
-					 min(editDistance(s1,s2,m,n-1),
-					 editDistance(s1,s2,m-1,n)));
+	int dp[m+1][n+1];
+	dp[0][0]=0;
+	for(int i=1;i<=m;i++) dp[i][0]=i;
+	for(int i=1;i<=n;i++) dp[0][i]=i;
+
+	for(int i=1;i<=m;i++)
+		for(int j=1;j<=n;j++)
+		{
+			if(s1[i-1]==s2[j-1])
+				dp[i][j]=dp[i-1][j-1];
+			else
+				dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+		}
+	return dp[m][n];
 }
 
 int main()
